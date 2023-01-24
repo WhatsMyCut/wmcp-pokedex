b7ececfef650f2ea72bd6529b55da0071651fdff
Mike Taylor <mike@whatsmycut.com>
2023-01-23 22:09:16 -0800

    stripe the table

--- src/common/ViewHistory/ViewHistory.tsx
+++ src/common/ViewHistory/ViewHistory.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
-import { useAppSelector } from '../../hooks';
+import { setSelected } from '../../store/pokemonSlice';
+import { useAppDispatch, useAppSelector } from '../../hooks';
 import './ViewHistory.scss';
 
 const ViewHistory = (): JSX.Element => {
   const history = useAppSelector((state) => state.pokemon.viewHistory);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     history;
@@ -14,10 +16,13 @@ const ViewHistory = (): JSX.Element => {
     <>
       <div className="header">View History</div>
       <div className="view-history">
-        {history.map((v) => (
-          <div key={v.name}>
+        {history.map((v, i) => (
+          <div
+            key={v.name + i}
+            className={i % 2 === 0 ? 'odd' : 'even'}
+            onClick={() => dispatch(setSelected(v))}
+          >
             {v.name}
-            <br />
           </div>
         ))}
       </div>
